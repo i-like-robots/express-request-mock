4df5703435d1daa106e39a88a258a0ec240b8f03
zkldi <ddhitone@gmail.com>
2021-04-02 09:51:31 +0100

    Add .d.ts file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { Request, Response, RequestHandler } from "express";
+import { RequestOptions } from "node-mocks-http";
+
+export default function (
+    callback: RequestHandler,
+    options: RequestOptions,
+    decorators: Record<string, unknown>
+): Promise<{ req: Request; res: Response, request: Request, response: Response }>;
+
